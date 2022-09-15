53ba0014d03ae469375a92b3acd3a65ecc56f74e
nelson <nelson.martins@epicje.pt>
2022-09-15 01:19:19 +0100

    Fluxo e Correções

---
+++ Welldonated-master/lib/app/pagina13.dart
@@ -0,0 +1,421 @@
+import 'package:flutter/material.dart';
+import 'package:weeldonatedproject/app/pagina10.dart';
+import 'package:weeldonatedproject/app/pagina18.dart';
+
+import 'Editpage.dart';
+import 'LowerAppBar.dart';
+import 'mainpage.dart';
+
+
+class pagina13 extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff1a237e),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => pagina18()));
+
+            },
+            child: Text(
+              'Editar Perfil',
+              style: TextStyle(
+                fontFamily: 'Segoi UI',
+                fontSize: 23,
+                fontWeight: FontWeight.w400,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: null,
+            child: Text(
+              'blankkkkkkk',
+              style: TextStyle(
+                color: Color(0xff1a237e),
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: null,
+            child: Text(
+              'blankkkkkk',
+              style: TextStyle(
+                color: Color(0xff1a237e),
+              ),
+            ),
+          ),
+          Builder(builder: (BuildContext context) {
+            return IconButton(
+              alignment: Alignment.centerRight,
+              icon: const Icon(
+                Icons.close,
+                size: 35.0,
+              ),
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => MainPage()));
+              },
+            );
+          }),
+        ],
+        elevation: 0.0,
+      ),
+      body: Container(
+        color: Color(0xff1a237e),
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Bombeiros Voluntários da Póvoa de Lanhoso',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    wordSpacing: 4.0,
+                    letterSpacing: 1.0,
+                    fontFamily: 'Poppins',
+                    fontSize: 27.0,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: 17,
+                ),
+                CircleAvatar(
+                  backgroundImage: AssetImage('bombeiros.png'),
+                  radius: 65,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                TextFormField(
+                  cursorColor: Colors.white,
+                  decoration: InputDecoration(
+                    isDense: true,
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 10,
+                    ),
+                    filled: true,
+                    fillColor: Color(0xff3949AB),
+                    hintText: "Nome da Instituição",
+                    hintStyle: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'Poppins',
+                      color: Colors.white,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1.3,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1.4,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    ),
+                  ),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontFamily: 'Poppins',
+                  ),
+                ), //NOME
+                SizedBox(
+                  height: 30,
+                ),
+                TextFormField(
+                  cursorColor: Colors.white,
+                  decoration: InputDecoration(
+                    isDense: true,
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 10,
+                    ),
+                    filled: true,
+                    fillColor: Color(0xff3949AB),
+                    hintText: "Email institucional",
+                    hintStyle: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'Poppins',
+                      color: Colors.white,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1.3,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1.4,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    ),
+                  ),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontFamily: 'Poppins',
+                  ),
+                ), //Email
+                SizedBox(
+                  height: 30,
+                ),
+                TextFormField(
+                  cursorColor: Colors.white,
+                  decoration: InputDecoration(
+                    isDense: true,
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 10,
+                    ),
+                    filled: true,
+                    fillColor: Color(0xff3949AB),
+                    hintText: "Telemóvel",
+                    hintStyle: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'Poppins',
+                      color: Colors.white,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1.3,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1.4,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    ),
+                  ),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontFamily: 'Poppins',
+                  ),
+                ), //telemóvel
+                SizedBox(
+                  height: 30,
+                ),
+                TextFormField(
+                  cursorColor: Colors.white,
+                  decoration: InputDecoration(
+                    isDense: true,
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 10,
+                    ),
+                    filled: true,
+                    fillColor: Color(0xff3949AB),
+                    hintText: "Morada",
+                    hintStyle: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'Poppins',
+                      color: Colors.white,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1.3,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1.4,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    ),
+                  ),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontFamily: 'Poppins',
+                  ),
+                ), //Morada
+                SizedBox(
+                  height: 30,
+                ),
+                TextFormField(
+                  cursorColor: Colors.white,
+                  decoration: InputDecoration(
+                    isDense: true,
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 10,
+                    ),
+                    filled: true,
+                    fillColor: Color(0xff3949AB),
+                    hintText: "Código Postal",
+                    hintStyle: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'Poppins',
+                      color: Colors.white,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1.3,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1.4,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    ),
+                  ),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontFamily: 'Poppins',
+                  ),
+                ), //codigo postal
+                SizedBox(
+                  height: 30,
+                ),
+                TextFormField(
+                  cursorColor: Colors.white,
+                  decoration: InputDecoration(
+                    isDense: true,
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 10,
+                    ),
+                    filled: true,
+                    fillColor: Color(0xff3949AB),
+                    hintText: "Descrição",
+                    hintStyle: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'Poppins',
+                      color: Colors.white,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1.3,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1.4,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    ),
+                  ),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontFamily: 'Poppins',
+                  ),
+                ), //Descrição
+                SizedBox(
+                  height: 30,
+                ),
+                TextFormField(
+                  cursorColor: Colors.white,
+                  decoration: InputDecoration(
+                    isDense: true,
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 10,
+                    ),
+                    filled: true,
+                    fillColor: Color(0xff3949AB),
+                    hintText: "Área de intervenção",
+                    hintStyle: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'Poppins',
+                      color: Colors.white,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1.3,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1.4,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    ),
+                  ),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontFamily: 'Poppins',
+                  ),
+                ), //Area intervenção
+                SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                  onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => Editpage()));
+                  },
+                  child: Text(
+                    'Gerir anúncios',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22.0,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xffef6C00),
+                    fixedSize: Size(180, 50),
+                  ),
+                ),
+                SizedBox(
+                  height: 60,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: Lowerappbar(),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xFFFF9800),
+        child: Icon(
+          Icons.add,
+        ),
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => pagina10()));
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
